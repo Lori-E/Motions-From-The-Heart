881452ae28dd8d82628a3f5057b618a47246240b
Lori-E <thelastpoeticdreamer@gmail.com>
2025-05-25 13:29:58 -0500

    Create Home.js

---
+++ Src/Componemts/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => (
+  <main style={{ padding: "1rem", textAlign: "center" }}>
+    <blockquote style={{ fontStyle: "italic", color: "#D6336C", marginBottom: "1rem", fontSize: "1.25rem" }}>
+      "There are no better gifts to be given than gifts from the heart"
+    </blockquote>
+    <p style={{ fontSize: "1.125rem" }}>Welcome to MOTIONS FROM THE HEART, where every gift is made with love and meaning.</p>
+  </main>
+);
+
+export default Home;
